d229940c0585c92f3b046058dfe7bc56593e31cd
HeroGamer153 <jshalin21@gmail.com>
2023-12-06 18:16:32 -0600

    added code to send video to backend but getting network errors

--- Back-End/server.py
+++ Back-End/server.py
@@ -1,8 +1,10 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request, jsonify
 from deepgram import Deepgram
+from flask_cors import CORS
 import json, asyncio
 
 app = Flask(__name__)
+CORS(app, origins=['http://10.195.103.203:8081', 'exp://10.195.103.203:8081'])
 
 DEEPGRAM_API_KEY = 'b52c494019d6621040e163b32c222aa2175db709'
 
@@ -32,10 +34,30 @@ async def get_transcription():
     response = response["results"]["channels"][0]["alternatives"][0]["transcript"]
     return response
 
+@app.route('/test', methods=['GET'])
+def test_endpoint():
+    return jsonify({'message': 'Test endpoint works!'})
+    
+@app.route('/', methods=['POST'])
+def transcribe():
+    try:
+        # Get the video URI from the request JSON
+        print('Received POST request:', request.data)  # Log the request data
+        video_file = request.files['video']
+        print(video_file)
+        # Perform transcription using the provided video URI
+        #return asyncio.run(get_transcription())
+        return video_file
 
-@app.route('/')
-def root():
-    return asyncio.run(get_transcription())
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+# The generic route for handling other methods (e.g., GET)
+@app.route('/', methods=['GET'])
+def handle_get_request():
+    return jsonify({'message': 'GET method not allowed'}), 405
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
--- Front-End/microexpression-detector/screens/VideoLibrary.js
+++ Front-End/microexpression-detector/screens/VideoLibrary.js
@@ -15,6 +15,46 @@ const VideoLibrary = ({ route }) => {
     setSelectedVideo(null);
   };
 
+  
+  
+  const handleTranscribePress = async () => {
+    console.log('button pressed')
+    try {
+
+      const formData = new FormData();
+      formData.append('video', {
+        uri: selectedVideo.uri,
+        type: 'video/*',
+        name: 'video.mov',
+      });
+
+      // Send a request to your backend with the selected video URI
+      console.log('Request URL:', 'http://127.0.0.1:8000');  // Replace with your actual backend URL
+      console.log('Request Payload:', JSON.stringify({ videoUri: selectedVideo.uri }));
+      const response = await fetch('http://127.0.0.1:8000', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        body: formData,
+      });
+      console.log('Request Status:', response.status);
+      console.log('Request Body:', await response.text());
+
+  
+      if (response.ok) {
+        // If the request was successful, you can handle the response here
+        const transcription = await response.json();
+        console.log('Transcription:', transcription);
+      } else {
+        // Handle errors if the request was not successful
+        console.error('Failed to transcribe video');
+      }
+    } catch (error) {
+      console.error('Error while transcribing:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
@@ -40,6 +80,11 @@ const VideoLibrary = ({ route }) => {
             <Video source={{ uri: selectedVideo.uri }} style={styles.fullScreenVideo} useNativeControls />
             <TouchableOpacity style={styles.closeButton} onPress={handleCloseModal}>
               <Text style={styles.closeButtonText}>Close</Text>
+            </TouchableOpacity>
+
+            {/* New button */}
+            <TouchableOpacity style={styles.closeButton} onPress={handleTranscribePress}>
+              <Text style={styles.closeButtonText}>Transcibe</Text>
             </TouchableOpacity>
           </View>
         </Modal>
